d3b71a0aac5d334b43f24a3ace6420a8d540e530
Max Newcomer <maxwnewcomer@gmail.com>
2024-11-26 15:27:48 -0600

    feat: repo init structure

--- cli/src/cmds/mod.rs
+++ cli/src/cmds/mod.rs
@@ -1,2 +1,3 @@
+pub(crate) mod init;
 pub(crate) mod setup;
 mod utils;
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,4 @@
-use clap::Command;
+use clap::{Arg, Command};
 
 mod cmds;
 
@@ -6,12 +6,31 @@ fn main() {
     let matches = Command::new("o7d")
         .version(env!("CARGO_PKG_VERSION"))
         .about("The o7d management tool")
-        .subcommand(Command::new("setup").about("Runs the setup process"))
+        .subcommand(Command::new("setup").about("Runs the setup process and installs dependencies"))
+        .subcommand(
+            Command::new("init")
+                .about("Initializes an `o7d` repository in the current directory")
+                .arg(
+                    Arg::new("environments")
+                        .short('e')
+                        .long("envs")
+                        .help("Specify custom environments (space-separated)")
+                        .num_args(1..)
+                        .value_delimiter(' '),
+                ),
+        )
         .get_matches();
 
     match matches.subcommand() {
         Some(("setup", _)) => {
             cmds::setup::run_setup();
+        }
+        Some(("init", sub_matches)) => {
+            let envs: Vec<&str> = sub_matches
+                .get_many::<String>("environments")
+                .map(|values| values.map(|s| s.as_str()).collect())
+                .unwrap_or_else(Vec::new);
+            cmds::init::init_dir(envs);
         }
         _ => {
             println!("Use --help for available commands");
